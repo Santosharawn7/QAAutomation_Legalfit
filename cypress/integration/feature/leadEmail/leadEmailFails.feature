e8ef0b27a47ef04aff38cf67a55c98c8e8ce75dc
santoshb7 <santosh@legalfit.com>
2022-06-01 22:52:16 -0400

    Changes implemented

--- cypress/integration/feature/leadEmail/leadEmailFails.feature
+++ cypress/integration/feature/leadEmail/leadEmailFails.feature
@@ -14,20 +14,20 @@ Scenario: Contact Form in the Hero Won't Submit If The Name, Email, or Descripti
 Scenario: Contact Form in the Footer Won't Submit If The Name, Email, or Description Is Missing
 
     Given I logged in on the Render version of the Premium Site and scroll to Footer Contact Form
-    When I fill every contents of form leaving name field empty on Footer Contact Form
-    | email | phone | case |
-    | testersgonnatest@gmail.com | 816-111-1111 | This is a test of the inquiry Contact Form. |
+    When I fill every contents of form leaving email field empty on Footer Contact Form
+    | name  | phone | case |
+    | John Test | 816-111-1111 | This is a test of the inquiry Contact Form. |
     And I press Footer Submit button
-    Then I should see "Please fill out this field." alert message under Name field.
+    Then I should see "Please fill out this field." alert message under Email field.
 
 Scenario: IP Contact Form Won't Submit If The Name, Email, or Description Is Missing
 
     Given I logged in on the Render version of the Interior page of Premium Site
-    When I fill every contents of form leaving name field empty on Interior Page
-    | email | phone | case |
-    | testersgonnatest@gmail.com | 816-111-1111 | This is a test of the inquiry Contact Form. |
+    When I fill every contents of form leaving inquiry field empty on Interior Page
+    | name | email | phone |
+    | John Test | testersgonnatest@gmail.com | 816-111-1111 |
     And I press Submit button on Interior page contact form
-    Then I should see "Please fill out this field." message under Name field of Interior page.
+    Then I should see "Please fill out this field." message under Inquiry field of Interior page.
 
 Scenario: Contact Page Form Won't Submit If The Name, Email, or Description Is Missing
 
@@ -53,11 +53,23 @@ Scenario: Contact Page Form Won't Submit If The Name, Email, or Description Is M
 Scenario: PPC Hero Contact Form Won't Submit If The Name, Email, or Description Is Missing
 
     Given I logged in on the Render version of the Premium Site having PPC Landing page and scroll to the Header contact form
-    When I fill every contents of form leaving name field empty on PPC landing Header form
+    When I fill every contents of form leaving name field empty on the hero block PPC landing Header form
     | email | phone | case |
     | testersgonnatest@gmail.com | 816-111-1111 | This is a test of the inquiry Contact Form. |
-    And I press Hero Submit button of the PPC Landing page
-    Then I should see "Please fill out this field." message under Name field of the PPC landing page
+    And I press Hero Submit button of the hero block PPC Landing page
+    Then I should see "Please fill out this field." message under Name field of the Hero block of PPC landing page
+    When I clear all the field and enter every details except email in Hero block of PPC Landing
+    | name | phone | case |
+    | John Test | 816-111-1111 | This is a test of the inquiry Contact Form. |
+    And I click the Submit button once again of Hero block in PPC Landing
+    Then I should see another alert Message in Hero block of PPC Landing page.
+    When I clear all the field and enter every details except Description in Hero block of PPC Landing page
+    | name | email | phone |
+    | John Test | testersgonnatest@gmail.com | 816-111-1111 |
+    And I click the Submit Button again in the Hero block of PPC Landing Page
+    Then I should see another alert Message in the Hero block of PPC Landing Page
+    When I clear all the field and click the submit button of the Hero block PPC Landing Page.
+    Then I will only see alert message of the hero block of the PPC Landing Page
 
 Scenario: PPC Footer Contact Form Won't Submit If The Name, Email, or Description Is Missing
 
@@ -67,3 +79,15 @@ Scenario: PPC Footer Contact Form Won't Submit If The Name, Email, or Descriptio
     | testersgonnatest@gmail.com | 816-111-1111 | This is a test of the inquiry Contact Form. |
     And I press Hero Submit button of the footer PPC landing form
     Then I should see "Please fill out this field." message under Name field of PPC landing footer form.
+    When I clear all the field and enter every details except email in the PPC Landing Page footer form
+    | name | phone | case |
+    | John Test | 816-111-1111 | This is a test of the inquiry Contact Form. |
+    And I click the Submit button once again on the footer of PPC landing page footer form
+    Then I should see another alert Message on the PPC Landing page footer form.
+    When I clear all the field and enter every details except Description in the PPC Landing footer form
+    | name | email | phone |
+    | John Test | testersgonnatest@gmail.com | 816-111-1111 |
+    And I click the Submit Button again of PPC Landing footer form
+    Then I should see another alert Message and form wont submit on PPC Landing footer form
+    When I clear all the field and click the submit button on PPC Landing footer form.
+    Then I will only see alert message on PPC landing footer form
--- cypress/integration/stepDefinitions/leadEmail/leadEmailfails.js
+++ cypress/integration/stepDefinitions/leadEmail/leadEmailfails.js
@@ -31,23 +31,23 @@ Given(`I logged in on the Render version of the Premium Site and scroll to Foote
     cy.visit('https://legal:fit@a-dominguez.builder.sandbox.legalfit.io')
 })
 
-When(`I fill every contents of form leaving name field empty on Footer Contact Form`, (datatable) => {
+When(`I fill every contents of form leaving email field empty on Footer Contact Form`, (datatable) => {
 datatable.hashes().forEach((element)=>{
-  cy.get('.footer [name=phone].form-control').scrollIntoView({force: true}).type(element.phone, {force: true})
-  cy.get('.footer [name=email].form-control').type(element.email, {force: true})
+  cy.get('.footer [name=name].form-control').scrollIntoView({force: true}).type(element.name, {force: true})
+  cy.get('.footer [name=email].form-control').clear({force: true})
+  cy.get('.footer [name=phone].form-control').type(element.phone, {force: true})
   cy.get('.footer [name=inquiry].form-control').type(element.case, {force: true})
   })
 })
 
 And(`I press Footer Submit button`, () => {
-    cy.get('.footer .f-inquiry-form [name=name].form-control').scrollIntoView({force: true})
+    cy.get('.footer .f-inquiry-form [name=email].form-control').scrollIntoView({force: true})
     cy.get('.footer [element=button]').click({force: true})
 })
 
-Then('I should see "Please fill out this field." alert message under Name field.',() => {
-   // cy.get('input:invalid').should('have.length', 3)
+Then('I should see "Please fill out this field." alert message under Email field.',() => {
     cy.get('.footer .f-inquiry-form').within(() => {
-        cy.get('[name=name]').invoke('prop', 'validationMessage')
+        cy.get('[name=email]').invoke('prop', 'validationMessage')
           .should('equal', 'Please fill out this field.')
     })
 })
@@ -57,11 +57,12 @@ Given(`I logged in on the Render version of the Interior page of Premium Site`,(
     cy.visit('https://legal:fit@a-dominguez.builder.sandbox.legalfit.io/practice-areas/drug-violations/marijuana-charges/')
 })
 
-When(`I fill every contents of form leaving name field empty on Interior Page`, (datatable) => {
+When(`I fill every contents of form leaving inquiry field empty on Interior Page`, (datatable) => {
     datatable.hashes().forEach((element)=>{
-    cy.get('.footer [name=phone].form-control').scrollIntoView({force: true}).type(element.phone, {force: true})
+    cy.get('.footer [name=name].form-control').scrollIntoView({force: true}).type(element.name, {force: true})
     cy.get('.footer [name=email].form-control').type(element.email, {force: true})
-    cy.get('.footer [name=inquiry].form-control').type(element.case, {force: true})
+    cy.get('.footer [name=phone].form-control').type(element.phone, {force: true})
+    cy.get('.footer [name=phone].form-control').clear({force: true})
     })
 })
 
@@ -70,10 +71,10 @@ And(`I press Submit button on Interior page contact form`, () => {
     cy.get('.footer [element=button]').click({force: true})
 })
 
-Then('I should see "Please fill out this field." message under Name field of Interior page.',() => {
+Then('I should see "Please fill out this field." message under Inquiry field of Interior page.',() => {
    // cy.get('input:invalid').should('have.length', 3)
     cy.get('.footer .f-inquiry-form').within(() => {
-        cy.get('[name=name]').invoke('prop', 'validationMessage')
+        cy.get('[name=inquiry]').invoke('prop', 'validationMessage')
           .should('equal', 'Please fill out this field.')
     })
 })
@@ -166,7 +167,7 @@ Given(`I logged in on the Render version of the Premium Site having PPC Landing 
     cy.visit('https://legal:fit@a-price.builder.sandbox.legalfit.io/family-law-attorney-tulsa/')
 })
 
-When(`I fill every contents of form leaving name field empty on PPC landing Header form`, (datatable) => {
+When(`I fill every contents of form leaving name field empty on the hero block PPC landing Header form`, (datatable) => {
 datatable.hashes().forEach((element)=>{
   cy.get('.landing-hero [name=phone].form-control').scrollIntoView({force: true}).type(element.phone, {force: true})
   cy.get('.landing-hero [name=email].form-control').type(element.email, {force: true})
@@ -174,16 +175,72 @@ datatable.hashes().forEach((element)=>{
   })
 })
 
-And(`I press Hero Submit button of the PPC Landing page`, () => {
+And(`I press Hero Submit button of the hero block PPC Landing page`, () => {
     cy.get('.landing-hero .f-inquiry-form [name=name].form-control').scrollIntoView({force: true})
     cy.get('.landing-hero [element=button]').click({force: true})
 })
 
-Then('I should see "Please fill out this field." message under Name field of the PPC landing page',() => {
+Then('I should see "Please fill out this field." message under Name field of the Hero block of PPC landing page',() => {
    // cy.get('input:invalid').should('have.length', 3)
     cy.get('.landing-hero .f-inquiry-form').within(() => {
         cy.get('[name=name]').invoke('prop', 'validationMessage')
           .should('equal', 'Please fill out this field.')
+    })
+When('I clear all the field and enter every details except email in Hero block of PPC Landing',(datatable)=>{
+    datatable.hashes().forEach((element)=>{
+        cy.get('.landing-hero [name=name].form-control').scrollIntoView({force: true}).type(element.name, {force: true})
+        cy.get('.landing-hero [name=phone].form-control').clear({force: true}).type(element.phone, {force: true})
+        cy.get('.landing-hero [name=email].form-control').clear({force: true})
+        cy.get('.landing-hero [name=inquiry].form-control').clear({force: true}).type(element.case, {force: true})
+        })
+    })
+    
+And('I click the Submit button once again of Hero block in PPC Landing', ()=> {
+    cy.get('.landing-hero .f-inquiry-form [name=email].form-control').scrollIntoView({force: true})
+    cy.get('.landing-hero [element=button]').click({force: true})
+})
+    
+Then('I should see another alert Message in Hero block of PPC Landing page.', ()=> {
+    cy.get('.landing-hero .f-inquiry-form').within(() => {
+        cy.get('[name=email]').invoke('prop', 'validationMessage')
+            .should('equal', 'Please fill out this field.')
+        })
+    })
+    
+When('I clear all the field and enter every details except Description in Hero block of PPC Landing page', (datatable)=> {
+    datatable.hashes().forEach((element)=>{
+        cy.get('.landing-hero [name=name].form-control').scrollIntoView({force: true}).clear({force: true}).type(element.name, {force: true})
+        cy.get('.landing-hero [name=phone].form-control').clear({force: true}).type(element.phone, {force: true})
+        cy.get('.landing-hero [name=email].form-control').clear({force: true}).type(element.email, {force: true})
+        cy.get('.landing-hero [name=inquiry].form-control').clear({force: true})
+        })
+    })
+    
+And('I click the Submit Button again in the Hero block of PPC Landing Page', () => {
+    cy.get('.landing-hero .f-inquiry-form [name=inquiry].form-control').scrollIntoView({force: true})
+    cy.get('.landing-hero [element=button]').click({force: true})
+    })
+    
+Then('I should see another alert Message in the Hero block of PPC Landing Page', ()=> {
+    cy.get('.landing-hero .f-inquiry-form').within(() => {
+        cy.get('[name=inquiry]').invoke('prop', 'validationMessage')
+            .should('equal', 'Please fill out this field.')
+        })
+    })
+    
+When('I clear all the field and click the submit button of the Hero block PPC Landing Page.', ()=> {
+    cy.get('.landing-hero [name=name].form-control').scrollIntoView({force: true}).clear({force: true})
+    cy.get('.landing-hero [name=phone].form-control').clear({force: true})
+    cy.get('.landing-hero [name=email].form-control').clear({force: true})
+    cy.get('.landing-hero [name=inquiry].form-control').clear({force: true})
+    cy.get('.landing-hero [element=button]').click({force: true})
+    })
+    
+Then('I will only see alert message of the hero block of the PPC Landing Page', ()=> {
+    cy.get('.landing-hero .f-inquiry-form').within(() => {
+        cy.get('[name=name]').invoke('prop', 'validationMessage')
+            .should('equal', 'Please fill out this field.')
+        })
     })
 })
 
@@ -210,5 +267,62 @@ Then('I should see "Please fill out this field." message under Name field of PPC
     cy.get('.landing-footer .f-inquiry-form').within(() => {
         cy.get('[name=name]').invoke('prop', 'validationMessage')
           .should('equal', 'Please fill out this field.')
+    })
+
+When('I clear all the field and enter every details except email in the PPC Landing Page footer form',(datatable)=>{
+    datatable.hashes().forEach((element)=>{
+        cy.get('.landing-footer [name=name].form-control').scrollIntoView({force: true}).type(element.name, {force: true})
+        cy.get('.landing-footer [name=phone].form-control').clear({force: true}).type(element.phone, {force: true})
+        cy.get('.landing-footer [name=email].form-control').clear({force: true})
+        cy.get('.landing-footer [name=inquiry].form-control').clear({force: true}).type(element.case, {force: true})
+        })
+    })
+    
+And('I click the Submit button once again on the footer of PPC landing page footer form', ()=> {
+    cy.get('.landing-footer .f-inquiry-form [name=email].form-control').scrollIntoView({force: true})
+    cy.get('.landing-footer [element=button]').click({force: true})
+    })
+    
+Then('I should see another alert Message on the PPC Landing page footer form.', ()=> {
+    cy.get('.landing-footer .f-inquiry-form').within(() => {
+        cy.get('[name=email]').invoke('prop', 'validationMessage')
+              .should('equal', 'Please fill out this field.')
+        })
+    })
+    
+When('I clear all the field and enter every details except Description in the PPC Landing footer form', (datatable)=> {
+    datatable.hashes().forEach((element)=>{
+        cy.get('.landing-footer [name=name].form-control').scrollIntoView({force: true}).clear({force: true}).type(element.name, {force: true})
+        cy.get('.landing-footer [name=phone].form-control').clear({force: true}).type(element.phone, {force: true})
+        cy.get('.landing-footer [name=email].form-control').clear({force: true}).type(element.email, {force: true})
+        cy.get('.landing-footer [name=inquiry].form-control').clear({force: true})
+        })
+    })
+    
+And('I click the Submit Button again of PPC Landing footer form', () => {
+    cy.get('.landing-footer .f-inquiry-form [name=inquiry].form-control').scrollIntoView({force: true})
+    cy.get('.landing-footer [element=button]').click({force: true})
+    })
+    
+Then('I should see another alert Message and form wont submit on PPC Landing footer form', ()=> {
+    cy.get('.landing-footer .f-inquiry-form').within(() => {
+        cy.get('[name=inquiry]').invoke('prop', 'validationMessage')
+            .should('equal', 'Please fill out this field.')
+        })
+    })
+    
+When('I clear all the field and click the submit button on PPC Landing footer form.', ()=> {
+    cy.get('.landing-footer [name=name].form-control').scrollIntoView({force: true}).clear({force: true})
+    cy.get('.landing-footer [name=phone].form-control').clear({force: true})
+    cy.get('.landing-footer [name=email].form-control').clear({force: true})
+    cy.get('.landing-footer [name=inquiry].form-control').clear({force: true})
+    cy.get('.landing-footer [element=button]').click({force: true})
+    })
+    
+Then('I will only see alert message on PPC landing footer form', ()=> {
+    cy.get('.landing-footer .f-inquiry-form').within(() => {
+        cy.get('[name=name]').invoke('prop', 'validationMessage')
+            .should('equal', 'Please fill out this field.')
+        })
     })
 })
