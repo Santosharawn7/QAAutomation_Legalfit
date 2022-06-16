64a4b92ea2722cb8ed87ae13a79faace7c89db14
phillip-legalfit <76116520+phillip-legalfit@users.noreply.github.com>
2022-06-16 14:48:26 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
     supportFile:'cypress/support/e2e.js',
     specPattern:
       'cypress/e2e/feature/',
-    baseUrl: 'http://sites.local.legalfit.io:8000',
+    baseUrl: 'http://sites.local.legalfit.io:8000/admin/edit',
     supportFile: 'cypress/support/e2e.js',
   },
 });
