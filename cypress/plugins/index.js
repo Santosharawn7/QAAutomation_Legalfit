835d22258445b0ff409b8cbc451299e9197937ba
santoshb7 <90635800+santoshb7@users.noreply.github.com>
2022-06-16 14:40:14 -0400

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,12 @@ module.exports = (on, config) => {
   const options = {
   }
   on('file:preprocessor', cucumber())
+   on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') { 
+      // ^ make sure this is your browser name, you may 
+      // be using 'canary' or 'chromium' for example, so change it to match!
+      args.push('--proxy-bypass-list=<-loopback>')
+      return args
+    }
+  })
 }
