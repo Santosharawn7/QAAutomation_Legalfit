2a64cef6ee94bb2342cc1e2109a945f180f9b870
santoshb7 <90635800+santoshb7@users.noreply.github.com>
2022-12-22 16:11:46 -0500

    extra loading removed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,7 +35,6 @@ Cypress.Commands.add('login', () => {
         login.email().type(data.local.email)
         login.password().type(data.local.password,{log: false})
         login.submitButton().click()
-        cy.visit(Cypress.env('localbaseUrl'))
         director.tableList().should('have.lengthOf.lte', 30)
     })
 })
